fc18971be39776ec61fcccffe868ad0ef0b53857
Theadedamola <adedamolajos@gmail.com>
2024-06-05 22:00:24 +0100

    - update navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,13 @@
 const Navbar = () => {
   return (
-    <div>
-        <p className="text-red-700">Logo</p>
+    <div className="h-20 my-6 mx-16 bg-black bg-opacity-40 backdrop-blur-xl rounded-3xl flex justify-between items-center p-10">
+      <div>
+        <p>Logo</p>
+        <img src="" alt="" />
+      </div>
+      <div className="bg-[#E14842] p-3 rounded-full text-white">
+        <button>Contact me</button>
+      </div>
     </div>
   )
 }
