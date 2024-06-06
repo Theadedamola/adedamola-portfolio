8b3a633eff3edf28197d36eeefd52494fdc341e9
Theadedamola <adedamolajose@gmail.com>
2024-06-06 18:20:48 +0100

    update hero button update navbar button

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,11 +5,11 @@ const Hero = () => {
     <div className="relative w-full h-screen bg-hero-bg bg-cover bg-no-repeat px-16 flex flex-col items-center pt-[130px]">
       <div className="z-10 absolute left-0 top-0 w-full h-screen bg-black bg-opacity-20 backdrop-blur-xl"></div>
       <div className="z-40 relative flex justify-between w-full">
-        <Link to='/work'>
-          <h1 className="text-5xl font-reyka text-white">Work.</h1>
+        <Link to="/work">
+          <h1 className="text-5xl font-reyka text-white">Work</h1>
         </Link>
-        <Link to='/about'>
-          <h1 className="text-5xl font-reyka text-white">About.</h1>
+        <Link to="/about">
+          <h1 className="text-5xl font-reyka text-white">About</h1>
         </Link>
       </div>
       <div className="z-40 relative mt-[100px] flex flex-col items-center space-y-4">
@@ -19,7 +19,10 @@ const Hero = () => {
           "The purpose of design is to make things better, not just look
           better." - Vitaly Friedman
         </p>
-        <Link to='/work' className="bg-black p-3 rounded-full text-white cursor-pointer">
+        <Link
+          to="/work"
+          className="bg-black p-3 rounded-full text-white cursor-pointer hover:shadow-2xl"
+        >
           <button>View my work</button>
         </Link>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,14 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      boxShadow: {
+        '3xl': 'inset 0 4px 4px rgba(7, 61, 148, 0.33)',
+      },
       backgroundImage: {
         'hero-bg': "url('/src/Components/assets/gradient-bg.jpeg')",
       },
       fontFamily: {
-        reyka: ['Reyka FREE', 'sans-serif'] 
+        reyka: ['Reyka FREE', 'sans-serif'],
       },
     },
   },
