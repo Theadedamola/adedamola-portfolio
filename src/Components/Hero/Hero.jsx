05d5443c58f7ddc62b458ce9afc711551aaadbe9
Theadedamola <adedamolajose@gmail.com>
2024-06-08 21:25:59 +0100

    -made landing page responsive

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,20 +5,24 @@ import Marquee from 'react-fast-marquee'
 
 const Hero = () => {
   return (
-    <div className="relative w-full h-screen bg-hero-bg bg-cover bg-no-repeat px-16 flex flex-col items-center pt-[130px]">
+    <div className="relative w-full h-screen bg-hero-bg bg-cover bg-no-repeat px-6 md:px-16 flex flex-col items-center pt-[130px]">
       <div className="z-10 absolute left-0 top-0 w-full h-screen bg-black bg-opacity-20 backdrop-blur-xl"></div>
       <div className="z-40 relative flex justify-between w-full">
         <Link to="/work">
-          <h1 className="text-5xl font-reyka text-white">Work.</h1>
+          <h1 className="text-4xl lg:text-5xl font-reyka text-white">Work.</h1>
         </Link>
         <Link to="/about">
-          <h1 className="text-5xl font-reyka text-white">About.</h1>
+          <h1 className="text-4xl lg:text-5xl font-reyka text-white">About.</h1>
         </Link>
       </div>
-      <div className="z-40 relative mt-[50px] flex flex-col items-center space-y-4">
-        <h1 className="text-7xl font-reyka text-white">Adedamola Alausa</h1>
-        <h1 className="text-7xl font-reyka text-white">UX Engineer</h1>
-        <p className="text-white">
+      <div className="z-40 relative w-full mt-[50px] flex flex-col items-center space-y-4">
+        <h1 className="text-[42px] sm:text-7xl font-reyka text-white">
+          Adedamola Alausa
+        </h1>
+        <h1 className="text-[42px] sm:text-7xl font-reyka text-white">
+          UX Engineer
+        </h1>
+        <p className="text-white lg:w-full text-center">
           "The purpose of design is to make things better, not just look
           better." - Vitaly Friedman
         </p>
@@ -30,7 +34,7 @@ const Hero = () => {
         </Link>
         <Marquee
           pauseOnHover
-          style={{ width: '50%', marginTop: '50px' }}
+          style={{ width: '80%', marginTop: '50px',  }}
           className="bg-black bg-opacity-25 backdrop-blur-sm rounded-3xl border border-[#2a2a2a31]"
         >
           <div className="flex space-x-10 px-6 py-10 ">
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <div className="relative">
-      <div className="fixed z-50 w-[90%] h-20 my-6 mx-16 bg-black bg-opacity-40 backdrop-blur-xl rounded-3xl flex justify-between items-center p-10 2xl:w-[95%]">
+      <div className="fixed z-50 w-[90%] md:w-[85%] lg:w-[90%] h-20 my-6 mx-6 md:mx-16 bg-black bg-opacity-40 backdrop-blur-xl rounded-3xl flex justify-between items-center p-10 2xl:w-[95%]">
         <div>
           <p>Logo</p>
           <img src="" alt="" />
