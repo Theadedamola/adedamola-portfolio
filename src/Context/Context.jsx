2271a9dfb06ccb9630b07c0c42c81ebd3061e036
Theadedamola <adedamolajose@gmail.com>
2024-06-13 17:23:26 +0100

    -projects data structure

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -1,9 +1,10 @@
 import { createContext, useEffect, useState } from 'react'
+import { designData } from '../Components/assets/designData'
 
 export const Context = createContext(null)
 
 const ContextProvider = (props) => {
-  const contextValue = { }
+  const contextValue = { designData }
   return (
     <Context.Provider value={contextValue}>{props.children}</Context.Provider>
   )
